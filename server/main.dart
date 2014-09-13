b58a47a4e9cdcf498243ae029e48bb59c10f4d9d
Alex Nichol <unixpickle@gmail.com>
2014-09-13 13:13:25 -0400

    separate "controller" and "slave" pages

--- server/main.dart
+++ server/main.dart
@@ -4,16 +4,29 @@ import 'dart:io';
 import 'package:web_router/web_router.dart';
 import 'package:path/path.dart' as path_library;
 
-String get clientDirectory {
+String projectDirectory(String name) {
   String scriptDir = path_library.dirname(Platform.script.path);
-  String abnormal = path_library.join(scriptDir, '../client');
+  String abnormal = path_library.join(scriptDir, '..', name);
   return path_library.normalize(abnormal);
 }
 
-String get packagesDirectory {
-  String scriptDir = path_library.dirname(Platform.script.path);
-  String abnormal = path_library.join(scriptDir, '../packages');
-  return path_library.normalize(abnormal);
+String get packagesDirectory => projectDirectory('packages');
+
+void setupRoutes(Router router, String name) {
+  // compile index.dart if necessary
+  router.add(new Dart2JSPathRoute('/$name/index.dart.js', 'GET', true,
+      path_library.join(projectDirectory(name), 'index.dart')));
+  
+  // serve index file
+  router.redirect('/$name', '/$name/');
+  router.staticFile('/$name/',
+      path_library.join(projectDirectory(name), 'index.html'));
+  
+  // serve packages directory
+  router.staticDirectory('/$name/packages', packagesDirectory);
+  
+  // serve the rest of the directory contents normally
+  router.staticDirectory('/$name', projectDirectory(name));
 }
 
 void main(List<String> args) {
@@ -25,12 +38,12 @@ void main(List<String> args) {
   int port = args.length > 0 ? int.parse(args[0]) : 1337;
   
   Router router = new Router();
-  router.add(new Dart2JSPathRoute('/index.dart.js', 'GET', true,
-      path_library.join(clientDirectory, 'index.dart')));
-  router.staticFile('/', path_library.join(clientDirectory, 'index.html'));
-  router.staticFile('', path_library.join(clientDirectory, 'index.html'));
-  router.staticDirectory('/packages', packagesDirectory);
-  router.staticDirectory('/', clientDirectory);
+  router.staticFile('', path_library.join(projectDirectory('server'),
+      'root_page.html'));
+  router.staticFile('/', path_library.join(projectDirectory('server'),
+      'root_page.html'));
+  setupRoutes(router, 'controller');
+  setupRoutes(router, 'slave');
   
   HttpServer.bind('localhost', port).then((HttpServer server) {
     print('Application listening on http://localhost:$port');
