ec9914474b644355f5ef98cd664df82eff22314d
Alex Nichol <unixpickle@gmail.com>
2014-09-13 11:29:57 -0400

    server and client work with web_router 0.0.3

--- server/main.dart
+++ server/main.dart
@@ -2,29 +2,38 @@ library web_play_server;
 
 import 'dart:io';
 import 'package:web_router/web_router.dart';
+import 'package:path/path.dart' as path_library;
 
-part 'static_route.dart';
+String get clientDirectory {
+  String scriptDir = path_library.dirname(Platform.script.path);
+  String abnormal = path_library.join(scriptDir, '../client');
+  return path_library.normalize(abnormal);
+}
 
-void main() {
-  String rootDirectory = '';
+String get packagesDirectory {
+  String scriptDir = path_library.dirname(Platform.script.path);
+  String abnormal = path_library.join(scriptDir, '../packages');
+  return path_library.normalize(abnormal);
+}
+
+void main(List<String> args) {
+  if (args.length == 1 && ['-h', '--help'].contains(args.first)) {
+    print('Usage: dart main.dart <port>');
+    exit(1);
+  }
+  
+  int port = args.length > 0 ? int.parse(args[0]) : 1337;
   
   Router router = new Router();
-  router.add(new StaticFileRoute(rootDirectory, 'GET', true, 
-      '../client/index.html', 'text/html'));
-  router.add(new StaticFileRoute(rootDirectory + '/', 'GET', true,
-      '../client/index.html', 'text/html'));
-  router.add(new StaticFileRoute(rootDirectory + '/index.dart', 'GET', true,
-      '../client/index.dart', 'application/dart'));
+  router.add(new Dart2JSPathRoute('/index.dart.js', 'GET', true,
+      path_library.join(clientDirectory, 'index.dart')));
+  router.staticFile('/', path_library.join(clientDirectory, 'index.html'));
+  router.staticFile('', path_library.join(clientDirectory, 'index.html'));
+  router.staticDirectory('/packages', packagesDirectory);
+  router.staticDirectory('/', clientDirectory);
   
-  HttpServer.bind('localhost', 1337).then((HttpServer server) {
-    server.listen((HttpRequest req) {
-      if (req.uri.path == rootDirectory + '/ws') {
-        WebSocketTransformer.upgrade(req).then((WebSocket websocket) {
-          websocket.listen(print);
-        });
-      } else {
-        router.httpHandler(req);
-      }
-    });
+  HttpServer.bind('localhost', port).then((HttpServer server) {
+    print('Application listening on http://localhost:$port');
+    server.listen(router.httpHandler);
   });
 }
