2347dcbdd072c95dcde6cce42414dbbe8d4c3b4a
Alex Nichol <unixpickle@gmail.com>
2014-09-15 18:38:53 -0400

    added script to generate package links

---
+++ gen-links.sh
@@ -0,0 +1,4 @@
+cd slave && ln -s ../packages ./packages && cd -
+cd controller && ln -s ../packages ./packages && cd -
+cd server && ln -s ../packages ./packages && cd -
+cd shared && ln -s ../packages ./packages && cd -
